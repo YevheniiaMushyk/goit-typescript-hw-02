40231a8e1f36c285851329a12c7f56b493eb6606
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-04-22 13:41:46 +0300

    Update ImageCard.tsx

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -4,7 +4,7 @@ import ImageCardProps from "./ImageCard.types";
 const ImageCard: React.FC<ImageCardProps> = ({ image, openModal }) => {
 	return (
 		<div className={css.imageContainer}>
-			<img className={css.image} src={image.urls.small} alt={image.alt_description} onClick={() => openModal(image)} />
+			<img className={css.image} src={image.urls.small} alt={image.alt_description || "Image"} onClick={() => openModal(image)} />
 		</div>
 	);
 };
